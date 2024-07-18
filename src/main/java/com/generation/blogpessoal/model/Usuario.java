0eecbcecda56aeac1b1167a895b54807c0fe5939
Wendy Souza <contato.wendy@hotmail.com>
2024-07-18 18:13:52 -0300

    Adicionando testes da classe UsuarioController

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -42,6 +42,16 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() { }
+	
 	public Long getId() {
 		return id;
 	}
@@ -90,5 +100,8 @@ public class Usuario {
 		this.postagem = postagem;
 	}
 	
-		
+	
+
+	
+	
 }
